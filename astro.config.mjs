69b688eb3de4f11c716900c2f74d9d6e4f68f5a2
Iris <sidorovf996@gmail.com>
2025-11-16 23:43:05 +0300

    Add cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "DM Sans",
-            cssVariable: "--font-dm-sans",
-            weights: [ 200, 500, 'bold' ]
-        }]
-    }
+  experimental: {
+      fonts: [{
+          provider: fontProviders.google(),
+          name: "DM Sans",
+          cssVariable: "--font-dm-sans",
+          weights: [ 200, 500, 'bold' ]
+      }]
+  },
+
+  adapter: cloudflare()
 });
